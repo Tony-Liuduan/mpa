2c5378ef34856307bed76d3da1dde4dd91dd5cd4
liuduan <liuduan@58.com>
2020-05-10 19:23:58 +0800

    update webpack config

--- config/webpack.development.js
+++ config/webpack.development.js
@@ -2,7 +2,7 @@
  * @fileoverview webpack development config
  * @author liuduan
  * @Date 2020-05-10 16:56:56
- * @LastEditTime 2020-05-10 19:17:34
+ * @LastEditTime 2020-05-10 19:22:25
  */
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -12,6 +12,10 @@ const webpackConfig = {
     plugins: [
         // 把layouts components复制到dist文件，这些是不需要打包的html模板文件
         new CopyWebpackPlugin([
+            {
+                from: path.join(__dirname, '../src/web/favicon.ico'),
+                to: '../favicon.ico',
+            },
             {
                 from: path.join(__dirname, '../src/web/views/layouts/layout.html'),
                 to: '../views/layouts/layout.html',
